b251553e49fed5983f57ffc393f45f221c6ec5b4
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-08-26 23:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+let arr = [1,2,3,4]
+let p1 = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res(arr.filter((x) => {
+            if(x%2==0){
+                return x
+            }
+        }))
+    }, 3000)
+})
+p1.then((res)=>{
+    // console.log(res)
+	document.write(res)
+    let y = res;
+    let p2 = new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(y.map((x) => {
+                return x*2
+            }))
+        }, 1000)
+    })
+
+    p2.then((res)=>{
+		// document.innerText = res
+        document.querySelector("body").innerText=""
+        document.write(res)
+        // console.log(res)
+    })
+    .catch(()=>{
+        console.log("Error in Promise 2")
+    })
+})
+.catch(()=>{
+    console.log("Promise rejected")
+})
